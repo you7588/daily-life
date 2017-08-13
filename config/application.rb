3c997ebefda50bada07cc007e7ecead7ed181949
you7588 <yo7588@gmail.com>
2017-08-13 18:14:12 +0800

    strftime

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module DailyLife
   class Application < Rails::Application
 
     config.i18n.default_locale = "zh-CN"
-    
+    config.time_zone = "Beijing"
+    Time::DATE_FORMATS.merge!(:default => '%Y/%m/%d %I:%M %p', :ymd => '%Y/%m/%d', :my => '%m/%d %k:%M %a ')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
