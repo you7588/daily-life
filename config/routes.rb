14bca8dbba1b7681cf9db9b9f6a7333842acf9ff
you7588 <yo7588@gmail.com>
2017-08-26 15:46:16 +0800

    add user to registration

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :orids do
     resources :posts
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822060718) do
+ActiveRecord::Schema.define(version: 20170826073651) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.text     "content"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20170822060718) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
