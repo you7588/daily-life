0d319de51a303585ddf76e6e491f0cbfc62fbd7d
you7588 <yo7588@gmail.com>
2017-08-26 17:03:28 +0800

    debug wrong words

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation]
     added_params = [:name, :email, :password, :password_confirmation, :image]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attres 
-    devise_parameter_sanitizer.permit :account_update, keys: assed_params  
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_params
   end
 end
